02e30a1b2cfbaf30890ed7582671216f15e88ac9
henneh <galvehannah5302@gmail.com>
2024-01-27 21:56:23 +0800

    updated db

--- api/models/calisuralaw.sql
+++ api/models/calisuralaw.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 10, 2024 at 12:31 PM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `calisuralaw`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `client`
---
+USE `beuma5l7exngin5pxpyt`;
 
 CREATE TABLE `client` (
   `client_id` bigint(20) NOT NULL,
@@ -35,11 +8,7 @@ CREATE TABLE `client` (
   `account_status` enum('active','deleted') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `record`
---
 
 CREATE TABLE `record` (
   `record_id` bigint(20) NOT NULL,
@@ -52,11 +21,6 @@ CREATE TABLE `record` (
   `client_id` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
 
 CREATE TABLE `user` (
   `user_id` bigint(20) NOT NULL,
@@ -65,62 +29,35 @@ CREATE TABLE `user` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `client`
---
+INSERT INTO `user` (`user_id`, `user_name`, `user_type`, `password`) VALUES
+(1, 'venus', 2, '$2a$12$1WJ0t3gtdYDT8oVpA9b/WevJBXb5ezKctV8JkJx2HqRZ8cliS3Zd.'),
+(2, 'attyjan', 1, '$2a$12$Mw825z.7zQDI1Bycicnp7.iHPs0yDy6VqAkyDhql/WOn6Bdfi7MEO');
+
 ALTER TABLE `client`
   ADD PRIMARY KEY (`client_id`);
 
---
--- Indexes for table `record`
---
+
 ALTER TABLE `record`
   ADD PRIMARY KEY (`record_id`),
   ADD KEY `client_id` (`client_id`);
 
---
--- Indexes for table `user`
---
+
 ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `client`
---
 ALTER TABLE `client`
   MODIFY `client_id` bigint(20) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `record`
---
+
 ALTER TABLE `record`
   MODIFY `record_id` bigint(20) NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT for table `user`
---
+
 ALTER TABLE `user`
   MODIFY `user_id` bigint(20) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `record`
---
 ALTER TABLE `record`
   ADD CONSTRAINT `record_ibfk_1` FOREIGN KEY (`client_id`) REFERENCES `client` (`client_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
